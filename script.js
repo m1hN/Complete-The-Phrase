340f57741dd6076b6ceaccbd1b99ea44bb3b683b
m1hN <145584972+m1hN@users.noreply.github.com>
2023-11-16 10:03:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("first").addEventListener("click", bu1);
+function bu1() {
+  alert("leg.");
+}
+
+document.getElementById("second").addEventListener("click", bu2);
+function bu2() {
+  alert("slack.");
+}
+
+document.getElementById("third").addEventListener("click", bu3);
+function bu3() {
+  alert("day.");
+}
+
+document.getElementById("fourth").addEventListener("click", bu4);
+function bu4() {
+  alert("devil.");
+}
+
+document.getElementById("fifth").addEventListener("click", bu5);
+function bu5() {
+  alert("weather.");
+}
